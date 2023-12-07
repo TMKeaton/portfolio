fd86ac8925d7101c4583efb084d4dfb0a0e8c6f0
ATLASHODL <113058617+TMKeaton@users.noreply.github.com>
2023-12-07 16:22:18 -0500

    index.php files and

---
+++ index.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Tyler Keaton | Portfolio</title>
+  <meta name="description" content="Personal portfolio of Web Designer and Developer Samir Alibabic">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="apple-touch-icon" href="apple-touch-icon.png">
+  <!-- Place favicon.ico in the root directory -->
+
+
+  <link rel="stylesheet" href="assets/css/index.css">
+
+
+  <link rel="stylesheet" href="https://samiralibabic.github.io/portfolio/css/normalize.css">
+
+  <!-- Google Font Lato -->
+  <link href='https://fonts.googleapis.com/css?family=Lato:400,300' rel='stylesheet' type='text/css'>
+
+  <!-- Font Awesome  -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://samiralibabic.github.io/portfolio/css/font-mfizz-2.3.0/font-mfizz.css">
+
+  <!-- Bootstrap -->
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  <script src="https://samiralibabic.github.io/portfolio/js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+
+<!--[if lt IE 8]>
+  <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+  <![endif]-->
+<header id="landing">
+  <nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-menu-items" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#landing"><span class="glyphicon glyphicon-home" aria-label="Home" aria-hidden="true"></span></a>
+      </div>
+      <div class="collapse navbar-collapse navbar-right" id="main-menu-items">
+        <ul class="nav navbar-nav">
+          <li><a href="#intro">About</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="hero-text-box">
+    <h1 id="main-heading">Tyler Keaton</h1>
+    <h2 id="main-subheading">[ Software Engineer ]</h2>
+    <a href="#contact" class="btn btn-default landing-btn">CONTACT</a>
+    <br><br>
+    <a href="https://proving.online/portfolio/assets/tyler-keaton-resume.pdf" target="_BLANK" class="btn btn-default landing-btn">RESUME</a>
+  </div>
+</header>
+<section id="intro" class="row">
+  <div class="col-sm-4 col-md-4 col-lg-4">
+    <h3 class="text-center"><span class="glyphicon glyphicon-user" aria-hidden="true"></span><br><br>INTRODUCTION</h3>
+    <p>
+      My name is Tyler I have 8 years experience developing using a wide variety of languages. Programming is more than a
+      career, it's a passion/hobby
+      I unceasingly try to improve.
+    </p>
+  </div>
+  <div class="col-sm-4 col-md-4 col-lg-4">
+    <h3 class="text-center"><span class="glyphicon glyphicon-blackboard" aria-hidden="true"></span><br><br>LANGUAGES</h3>
+    <p>
+      iOS-Swift
+      | HTML5 | CSS3
+      <br>JQuery
+      | Python | Ruby
+      <br>PHP | SQL
+      | Unreal Engine
+      <br>C# | C++
+      | Linux | Unix
+    </p>
+  </div>
+  <div class="col-sm-4 col-md-4 col-lg-4">
+    <h3 class="text-center"><span class="glyphicon glyphicon-education" aria-hidden="true"></span><br><br>EDUCATION</h3>
+    <p>
+      I graduated from St. Petersburg College with an A.S. degree for Computer Programming &
+      Analysis while maintaining a 3.8 GPA and a spot on the Deans list.
+    </p>
+  </div>
+</section>
+<section id="projects" class="row text-center">
+  <h3 class="text-left h3-underline col-xs-10 col-xs-offset-1">
+    PROJECTS
+  </h3>
+  <br>
+  <div class="col-xs-12 col-sm-6 col-md-3 project-box">
+    <a href="https://proving.online/portfolio/plasmacoin/" class="thumbnail" target="_blank">
+      <span class="glyphicon glyphicon-bitcoin" aria-hidden="true"></span>
+      &nbsp;Plasma Coin
+    </a>
+  </div>
+  <div class="col-xs-12 col-sm-6 col-md-3 project-box">
+    <a href="https://proving.online/portfolio/shop/" class="thumbnail" target="_blank">
+      <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>
+      &nbsp;Simple Web Store
+    </a>
+  </div>
+  <div class="col-xs-12 col-sm-6 col-md-3 project-box">
+    <a href="https://proving.online/form.php" class="thumbnail" target="_blank">
+      <span class="glyphicon glyphicon-comment" aria-hidden="true"></span>
+      &nbsp;Contact Form
+    </a>
+  </div>
+  <div class="col-xs-12 col-sm-6 col-md-3 project-box">
+    <a href="https://proving.online/portfolio/player/" class="thumbnail" target="_blank">
+      <span class="glyphicon glyphicon-headphones" aria-hidden="true"></span>
+      &nbsp;Music Player
+    </a>
+  </div>
+</section>
+<section id="skills" class="row">
+  <div class="col-xs-10 col-xs-offset-1">
+    <h3 class="text-center">
+      <i class="fa fa-trophy fa-2x" aria-hidden="true"></i><br><br>
+      CERTIFICATES
+    </h3>
+
+    <p class="text-center">
+      [ Responsive Web Design ]
+      <br>
+      [ Programming Specialist ]
+    </p>
+    <br />
+    <div class="row text-center">
+
+    </div>
+  </div>
+</section>
+<footer id="contact" class="row">
+  <h3 class="text-center">
+    <i class="fa fa-address-card fa-4x" aria-hidden="true"></i><br>
+    CONTACT
+  </h3>
+  <div class="col-xs-10 col-xs-offset-1 text-center">
+    <p>
+      I'd be happy to answer any questions you might have (or to have a coffee together) so feel free to contact me: <br><br> work.tkeaton@gmail.com
+    </p>
+
+    <a href="mailto:work.tkeaton@gmail.com" target="_BLANK">
+      <i class="fa fa-envelope" aria-hidden="true"></i>
+    </a>
+    <a href="https://facebook.com/sonotasynth" target="_BLANK">
+      <i class="fa fa-facebook" aria-hidden="true"></i>
+    </a>
+    <a href="https://github.com/TMKeaton" target="_BLANK">
+      <i class="fa fa-github" aria-hidden="true"></i>
+    </a>
+
+  </div>
+
+  <br>
+
+</footer>
+
+<script src="assets/js/index.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script src="https://samiralibabic.github.io/portfolio/js/plugins.js"></script>
+<script src="https://samiralibabic.github.io/portfolio/js/main.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+</html>
